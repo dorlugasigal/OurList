b2de9da1d94f03ff218747ac9352d9a6922f53a3
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2020-07-10 22:40:38 +0300

    :fire: remove redundant code

--- src/context/CreateDataContext.js
+++ src/context/CreateDataContext.js
@@ -1,5 +1,4 @@
 import React, { useReducer } from "react"
-import { ActionSheetIOS } from "react-native";
 
 export default (reducer, actions, initialState) => {
     const Context = React.createContext();
